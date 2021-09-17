eb4c569401c22f4ff076afa3887c73cbed214936
Michał Pierzchalski <pierzcham@umk.pl>
2021-09-17 22:38:50 +0200

    zmiany robocze

--- xpp_workspace.sh
+++ xpp_workspace.sh
@@ -1,14 +1,3 @@
 i3-msg "workspace 3; append_layout /home/michal/Dropbox/Research/xppstudio/workspace_xpp.json"
 i3-msg "exec emacs -q -l /home/michal/Dropbox/Research/xppstudio/emacs-xpp --name emacs-xpp"
-# i3-msg "exec urxvt -name \"nodemon-container\" -e inotify-hookable -d -f /home/michal/Dropbox/Research/xppstudio/xppcontrol.ode -c \"xppaut /home/michal/Dropbox/Research/xppstudio/xppcontrol.ode\""
-
 i3-msg "exec urxvt -name \"koszyk\" -e inotify-hookable -d -f /home/michal/Dropbox/Research/xppstudio/xppcontrol.ode -c \"xppaut /home/michal/Dropbox/Research/xppstudio/xppcontrol.ode\""
-
-# i3-msg "exec urxvt -name \"koszyk\" -e inotify-hookable -d -f /home/michal/Dropbox/Research/ukladydynamiczneGit/dynsys2d.ode -c \"xppaut /home/michal/Dropbox/Research/ukladydynamiczneGit/dynsys2d.ode\""
-
-# urxvt -name "nodemon-container" -e inotify-hookable -d -f /home/michal/Dropbox/Research/ukladydynamiczneGit/dynsys2d.ode -c "xppaut /home/michal/Dropbox/Research/ukladydynamiczneGit/dynsys2d.ode"
-
-# urxvt -name "nodemon-container" -e inotify-hookable -d -f /home/michal/Dropbox/Research/ukladydynamiczneGit/xppcontrol.ode -c "xppaut /home/michal/Dropbox/Research/ukladydynamiczneGit/xppcontrol.ode"
-# urxvt -name "nodemon-container" -e inotify-hookable -d -f /home/michal/Dropbox/Research/tenkapitanGit/dynsys2d.ode -c "xppaut /home/michal/Dropbox/Research/tenkapitanGit/dynsys2d.ode"
-# urxvt -name "nodemon-container" -e inotify-hookable -d -f /home/michal/Lectures/dynsys2d.ode -c "xppaut /home/michal/Lectures/dynsys2d.ode"
-# urxvt -name "nodemon-container" -e inotify-hookable -d -t 1000 -f /home/michal/Dropbox/Research/xppstudio/dynsys2d.ode -c "xppaut /home/michal/Dropbox/Research/xppstudio/dynsys2d.ode"
